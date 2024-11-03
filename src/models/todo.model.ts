a1f65a98286532592722621ec19752e5928b6839
labody <applemafia69@yahoo.com>
2024-11-03 13:36:54 +0000

    create todo model

---
+++ src/models/todo.model.ts
@@ -0,0 +1,39 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'boolean',
+    default: false,
+  })
+  isComplete?: boolean;
+
+  @property({
+    type: 'string',
+  })
+  description?: string;
+
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
+
+export interface TodoRelations {
+  // describe navigational properties here
+}
+
+export type TodoWithRelations = Todo & TodoRelations;
